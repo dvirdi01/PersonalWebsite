e173953c65a6d51b36d253f458ae5233b10ce4e9
dvirdi01 <virdidivjot@gmail.com>
2025-06-08 18:58:32 -0700

    added particles animation

---
+++ PersonalWebsite/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "tsparticles-slim"; // Slim version for smaller bundle
+import ParticlesComponent from "./Particles";
+
+const Hero = () => {
+
+  const text =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...";
+
+
+  return (
+    <div className="relative w-full min-h-screen flex items-center justify-center">
+        <div className="absolute top-0 left-0 w-full h-full z--1 opacity-1/2">
+            <ParticlesComponent id="particles" />
+        </div>
+        <div className="z-10 bg-orange-200 p-4 w-1/2 flex flex-col items-center justify-center rounded shadow-lg">
+            <h1 className="text-2xl font-bold mb-4">Heading</h1>
+            <p>{text}</p>
+        </div>
+    </div>
+  );
+};
+
+export default Hero;
--- PersonalWebsite/src/components/Navbar.jsx
+++ PersonalWebsite/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-white w-full flex flex-row items-center justify-between px-4 z-4 sticky'>
+    <div className='bg-blue-200 w-full flex flex-row items-center justify-between px-4 z-4 sticky'>
         <p> Portfolio </p>
         <div className='flex flex-row items-center space-between gap-5 p-4'>
             <p>About Me</p>
