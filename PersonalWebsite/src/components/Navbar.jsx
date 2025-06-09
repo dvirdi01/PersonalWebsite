6dd7590fd4cde1dd1f74b52bb7686ab201e1c74c
dvirdi01 <virdidivjot@gmail.com>
2025-06-08 17:25:25 -0700

    made navbar

---
+++ PersonalWebsite/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white w-full flex flex-row items-center justify-between px-4 z-4 sticky'>
+        <p> Portfolio </p>
+        <div className='flex flex-row items-center space-between gap-5 p-4'>
+            <p>About Me</p>
+            <p> Work</p>
+            <p> Projects</p>
+            <p> Contact</p>
+            <button> Resume</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
