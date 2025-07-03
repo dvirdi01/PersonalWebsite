c48ba9a7d9ef8ae4727973dbed35cc9e0ab110ca
dvirdi01 <virdidivjot@gmail.com>
2025-07-02 21:42:10 -0700

    changed contact form colors

--- PersonalWebsite/src/components/CommonButton.jsx
+++ PersonalWebsite/src/components/CommonButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const CommonButton = ({label}) => {
-    const borderStyle = "ease absolute border-[#58a6ff] transition-all duration-100 group-hover:w-full"
+    const borderStyle = "ease absolute border-[#58a6ff] transition-all duration-100 group-hover:w-full bg-[#f7e2c7]"
   return (
-    <button className='group relative inline-block overflow-hidden px-8 py-3 text-sm font-medium text-white hover:text-[#58a6ff] focus:outline-none focus:ring active:bg-indigo-600 active:text-white'>
-        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-1 border-[#4E4FEB] transition-all duration-100 group-hover:w-full"></span>
-	    <span className="ease absolute right-0 top-0 h-0 w-0 border-r-1 border-[#4E4FEB] transition-all duration-100 group-hover:h-full"></span>
-	    <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-1 border-[#4E4FEB] transition-all duration-100 group-hover:w-full"></span>
-	    <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-1 border-[#4E4FEB] transition-all duration-100 group-hover:h-full"></span>
+    <button className='group relative inline-block overflow-hidden px-8 py-3 text-sm font-medium text-[#f7e2c7] hover:text-[#f7e2c7] focus:outline-none focus:ring active:bg-indigo-600 active:text-white'>
+        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-1 border-[#f7e2c7] transition-all duration-100 group-hover:w-full"></span>
+	    <span className="ease absolute right-0 top-0 h-0 w-0 border-r-1 border-[#f7e2c7] transition-all duration-100 group-hover:h-full"></span>
+	    <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-1 border-[#f7e2c7] transition-all duration-100 group-hover:w-full"></span>
+	    <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-1 border-[#f7e2c7] transition-all duration-100 group-hover:h-full"></span>
 	    {label}
     </button>
   )
--- PersonalWebsite/src/components/ContactForm.jsx
+++ PersonalWebsite/src/components/ContactForm.jsx
@@ -5,19 +5,19 @@ const ContactForm = () => {
     return (
         <form className="max-w-sm mx-auto min-h-screen">
             <div className="mb-5">
-                <label for="name" className="block text-sm mb-2 font-medium text-white ">Your Name </label>
-                <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Awesome Stranger" required />
+                <label for="name" className="block text-sm mb-2 font-medium text-[#f7e2c7] ">Your Name </label>
+                <input type="text" id="name" className="bg-[#f7e2c7] border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Awesome Stranger" required />
             </div>
             <div className="mb-5">
-                <label for="email" className="block text-sm mb-2 font-medium text-white ">Your Email</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="yourname@gmail.com" required />
+                <label for="email" className="block text-sm mb-2 font-medium text-[#f7e2c7] ">Your Email</label>
+                <input type="email" id="email" className="bg-[#f7e2c7] border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="yourname@gmail.com" required />
             </div>
             <div className="mb-5">
-                <label for="message" className="block w-full mb-2 text-sm font-medium text-white">Message </label>
-                <textarea id="message" rows="5" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Type your message here..." required></textarea>
+                <label for="message" className="block w-full mb-2 text-sm font-medium text-[#f7e2c7]">Message </label>
+                <textarea id="message" rows="5" className="bg-[#f7e2c7] border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Type your message here..." required></textarea>
             </div>
             <div className='max-w-sm flex items-center justify-center'>
-                <CommonButton label="Submit"/>
+                <CommonButton label="Submit" className="bg-[#f7e2c7]"/>
             </div>
             {/* <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button> */}
         </form>
